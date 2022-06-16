3f358a22daa9d92f5c7a1e309d957b7dde30f451
Sarthak <sarthak.chaurasia@triconinfotech.com>
2022-06-16 22:00:22 +0530

    added temp routes for testing

--- src/App.js
+++ src/App.js
@@ -6,9 +6,25 @@ import Product from './pages/Product';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Cart from './pages/Cart';
+import {BrowserRouter as Router,Link,Routes,Route} from "react-router-dom"
 function App() {
   return (
-   <Cart/>
+   <Router>
+    <Link to="/"></Link>
+    <Link to="/cart"></Link>
+    <Link to="/login"></Link>
+    <Link to="/register"></Link>
+    <Link to="/singleproduct"></Link>
+    <Link to="/multipleproduct"></Link>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/cart' element={<Cart/>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/register' element={<Register/>}></Route>
+      <Route path='/singleproduct' element={<Product/>}></Route>
+      <Route path='/multipleproduct' element={<Productlist/>}></Route>
+    </Routes>
+   </Router>
   );
 }
 
